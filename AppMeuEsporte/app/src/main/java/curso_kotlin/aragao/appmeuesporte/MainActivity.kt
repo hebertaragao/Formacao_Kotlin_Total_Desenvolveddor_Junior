299cb693618acb8c930ac95235732811666bb71e
hebertaragao <hebert.aragao@outlook.com>
2023-04-04 22:41:34 -0300

    Primeiro commit do exercicio Minhas Viagens

---
+++ AppMeuEsporte/app/src/main/java/curso_kotlin/aragao/appmeuesporte/MainActivity.kt
@@ -0,0 +1,11 @@
+package curso_kotlin.aragao.appmeuesporte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
